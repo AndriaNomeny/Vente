4c21ada691b4d299ae09f239cb3fd264e4fa378f
NyAvotraRak <ny.avotra.rak@gmail.com>
2025-03-24 10:23:02 +0300

    aona

--- app/Http/Controllers/produitController.php
+++ app/Http/Controllers/produitController.php
@@ -14,6 +14,7 @@ class produitController extends Controller
 {
     public function a()
     {
+        //bobota
         // User::create([ssdssd
         //     'name' => 'Nomeny',
         //     'email'=> 'Nomeny@gmail.com',
